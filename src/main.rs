e2513c1720afae7636cdc7fad2bc4ad3bc7cdb78
AAPL00 <aapldev00@proton.me>
2026-04-15 11:43:34 -0400

    Add remove_duplicates_from_sorted_array

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod palindrome_number_9;
 mod roman_to_integer_13;
 mod longest_common_prefix_14;
 mod valid_parentheses_20;
+mod remove_duplicates_from_sorted_array_26;
 fn main() {
     println!("Hello, world!");
 }
---
+++ src/remove_duplicates_from_sorted_array_26.rs
@@ -0,0 +1,16 @@
+fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
+    if nums.is_empty() {
+        return 0;
+    }
+
+    let mut write = 1;
+
+    for read in 1..nums.len() {
+        if nums[read] != nums[read - 1] {
+            nums[write] = nums[read];
+            write += 1;
+        }
+    }
+
+    write as i32
+}
